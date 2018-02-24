4fec37196c1d3c176602f85c60afb3ec129abf1f
Duncan McGreggor <oubiwann@gmail.com>
2018-02-24 13:17:37 -0600

    Added dev ns.

---
+++ dev-resources/src/clojusc/blogger/dev.clj
@@ -0,0 +1,8 @@
+(ns clojusc.blogger.dev
+  (:require
+    [clojusc.blogger.blog :as blog]
+    [clojusc.blogger.comment :as comment]
+    [clojusc.blogger.page :as page]
+    [clojusc.blogger.post :as post]
+    [clojusc.blogger.user :as blog-user]))
+
