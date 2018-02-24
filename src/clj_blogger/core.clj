b89540d8f529d373421209e78819dfb070c6fb4f
Duncan McGreggor <oubiwann@gmail.com>
2018-02-24 09:30:27 -0600

    Initial stubbs for project.

---
+++ src/clj_blogger/core.clj
@@ -0,0 +1,6 @@
+(ns clj-blogger.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/clj_blogger/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-blogger.core-test
+  (:require [clojure.test :refer :all]
+            [clj-blogger.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
