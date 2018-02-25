890506d2d989564f39a8528af479d8d637e48e24
Duncan McGreggor <oubiwann@gmail.com>
2018-02-25 14:14:39 -0600

    Added some utility functions.

--- src/clojusc/blogger/util.clj
+++ src/clojusc/blogger/util.clj
@@ -1,4 +1,15 @@
 (ns clojusc.blogger.util
   (:require
-    [cheshire.core :as json]))
+    [cheshire.core :as json]
+    [clojure.java.io :as io]))
 
+(defn read-json
+  [filename]
+  (if filename
+    (json/parse-stream (io/reader filename) true)))
+
+(defn get-args
+  [client args]
+  (merge
+    (:config client)
+    args))
