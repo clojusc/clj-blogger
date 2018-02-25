fa5bdd764058dceeaf912bbbd1aae9e5b10ac0f9
Duncan McGreggor <oubiwann@gmail.com>
2018-02-24 23:36:07 -0600

    Updated API.

--- dev-resources/src/clojusc/blogger/dev.clj
+++ dev-resources/src/clojusc/blogger/dev.clj
@@ -1,8 +1,13 @@
 (ns clojusc.blogger.dev
   (:require
-    [clojusc.blogger.blog :as blog]
-    [clojusc.blogger.comment :as comment]
-    [clojusc.blogger.page :as page]
-    [clojusc.blogger.post :as post]
-    [clojusc.blogger.user :as blog-user]))
+    [cheshire.core :as json]
+    [clj-oauth2.client :as oauth2]
+    [clojusc.blogger.api.impl.blog :as blog]
+    [clojusc.blogger.api.impl.comment :as comment]
+    [clojusc.blogger.api.impl.page :as page]
+    [clojusc.blogger.api.impl.post :as post]
+    [clojusc.blogger.api.impl.user :as blog-user]
+    [clojusc.blogger.auth :as auth]
+    [clojusc.blogger.routes :as routes]
+    [clojusc.blogger.util :as util]))
 
