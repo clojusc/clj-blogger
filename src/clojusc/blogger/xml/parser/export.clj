2e5a8ca8c691eb3556bbff691d4a51f5feb9d685
Duncan McGreggor <oubiwann@gmail.com>
2019-01-31 00:28:46 -0500

    Added author-extraction support.

--- src/clojusc/blogger/xml/parser/export.clj
+++ src/clojusc/blogger/xml/parser/export.clj
@@ -1,66 +1,76 @@
 (ns clojusc.blogger.xml.parser.export
-	"Functions for parsing XML export data from blogger."
-	(:require
-		[clojure.data.xml :as xml]
-		[clojure.data.zip.xml :as zip-xml]
-		[clojure.java.io :as io]
-		[clojure.zip :as zip]
-		[taoensso.timbre :as log])
-	(:import
-		(java.net URLEncoder)))
+  "Functions for parsing XML export data from blogger."
+  (:require
+    [clojure.data.xml :as xml]
+    [clojure.data.zip.xml :as zip-xml]
+    [clojure.instant :as instant]
+    [clojure.java.io :as io]
+    [clojure.zip :as zip]
+    [taoensso.timbre :as log])
+  (:import
+    (java.net URLEncoder)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;   Constants   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def atom-ns "http://www.w3.org/2005/Atom")
+(def atom-app-ns "http://purl.org/atom/app#")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;   General Utility Functions   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn ns-tag
-	[tag]
-	(if (keyword? tag)
-		(keyword
-			(format "xmlns.%s/%s"
-				      (URLEncoder/encode (str atom-ns) "UTF-8")
-				      (URLEncoder/encode (name tag) "UTF-8")))
-		tag))
+  ([tag]
+    (ns-tag atom-ns tag))
+  ([xml-ns tag]
+    (if (keyword? tag)
+      (keyword
+        (format "xmlns.%s/%s"
+                (URLEncoder/encode (str xml-ns) "UTF-8")
+                (URLEncoder/encode (name tag) "UTF-8")))
+      tag)))
 
 (defn xml->zip
-	[input]
-	(-> input
-		  io/input-stream
-		  xml/parse
-		  zip/xml-zip))
+  [input]
+  (-> input
+      io/input-stream
+      xml/parse
+      zip/xml-zip))
 
 (defn xml-resource->zip
-	[resource-file]
-	(xml->zip (io/resource resource-file)))
+  [resource-file]
+  (xml->zip (io/resource resource-file)))
 
 (defn xml->
-	[data tags]
-	(apply zip-xml/xml-> (cons data (mapv ns-tag tags))))
+  ([data tags]
+    (xml-> atom-ns data tags))
+  ([xml-ns data tags]
+    (xml-> #'zip-xml/xml-> xml-ns data tags))
+  ([func xml-ns data tags]
+    (apply func (cons data (mapv #(ns-tag xml-ns %) tags)))))
 
 (defn xml1->
-	[data tags]
-	(apply zip-xml/xml1-> (cons data (mapv ns-tag tags))))
+  ([data tags]
+    (xml1-> atom-ns data tags))
+  ([xml-ns data tags]
+    (xml-> #'zip-xml/xml1-> xml-ns data tags)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;   Blogger Utility Functions   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn is-post?
-	[id]
-	(re-matches #"tag:blogger.com.*\.post-.*" id))
+  [id]
+  (re-matches #"tag:blogger.com.*\.post-.*" id))
 
 (defn entry-id?
-	[id]
-	(fn [loc]
-		(let [current-id (xml1-> loc [:id zip-xml/text])]
-			(log/debug "Comparing " current-id " with " id " ... Equal? " (= current-id id))
-			(= current-id id))))
+  [id]
+  (fn [loc]
+    (let [current-id (xml1-> loc [:id zip-xml/text])]
+      (log/debug "Comparing " current-id " with " id " ... Equal? " (= current-id id))
+      (= current-id id))))
 
 (defn content
   [predicate]
@@ -73,27 +83,59 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn print-entry-ids
-	[xml-zipped-data]
-	(doall
-		(for [id (xml-> xml-zipped-data [:entry :id zip-xml/text])]
-			(println id)))
-	:ok)
+  [xml-zipped-data]
+  (doall
+    (for [id (xml-> xml-zipped-data [:entry :id zip-xml/text])]
+      (println id)))
+  :ok)
 
 (defn print-post-ids
-	[xml-zipped-data]
-	(doall
-		(for [id (xml-> xml-zipped-data [:entry :id zip-xml/text])]
-			(when (is-post? id)
-				(println id))))
-	:ok)
+  [xml-zipped-data]
+  (doall
+    (for [id (xml-> xml-zipped-data [:entry :id zip-xml/text])]
+      (when (is-post? id)
+        (println id))))
+  :ok)
 
 (defn print-post-ids
-	[xml-zipped-data]
-	(doall
-		(for [id (xml-> xml-zipped-data [:entry :id (content is-post?)])]
-			(println id)))
-	:ok)
+  [xml-zipped-data]
+  (doall
+    (for [id (xml-> xml-zipped-data [:entry :id (content is-post?)])]
+      (println id)))
+  :ok)
 
 (defn get-entry
-	[xml-zipped-data id]
-	(xml-> xml-zipped-data [:entry (entry-id? id) zip/children]))
+  [xml-zipped-data id]
+  (xml-> xml-zipped-data [:entry (entry-id? id) zip/node]))
+
+(defn extract-tag-text
+  [parsed-coll & tags]
+  (log/debug "Extracting text from tags:" tags)
+  (xml1-> (apply zip/xml-zip parsed-coll) (conj (vec tags) zip-xml/text)))
+
+(defn extract-draft-bool
+  [parsed-coll]
+  (when-let [bool-str (xml1-> atom-app-ns
+                              (apply zip/xml-zip parsed-coll)
+                              [:control :draft zip-xml/text])]
+    (= bool-str "yes")))
+
+(defn extract-author
+  [parsed-coll]
+  {:name (extract-tag-text parsed-coll :author :name)
+   :email (extract-tag-text parsed-coll :author :email)
+   :uri (extract-tag-text parsed-coll :author :uri)})
+
+(defn extract-entry
+  [parsed-coll]
+  (let [title (extract-tag-text parsed-coll :title)]
+    {:id (extract-tag-text parsed-coll :id)
+     :title title
+     :published (instant/read-instant-date
+                 (extract-tag-text parsed-coll :published))
+     :updated (instant/read-instant-date
+               (extract-tag-text parsed-coll :updated))
+     :author (extract-author parsed-coll)
+     :draft? (extract-draft-bool parsed-coll)
+     :content (extract-tag-text parsed-coll :content)}))
+
